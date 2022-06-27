4cd8b7c0f753ce81c6ce70c4744f0829680c1136
Dheeraj <54492177+drj09@users.noreply.github.com>
2022-06-27 22:51:14 +0530

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+var React = require("react");
+var ReactDOM = require("react-dom");
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
