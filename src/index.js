9283184942ec289b011dba3ce3a280497a780722
Dheeraj <54492177+drj09@users.noreply.github.com>
2022-06-27 23:15:25 +0530

    added css as jsx

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Drake";
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
 ReactDOM.render(
   <div>
     <h1 className="heading">Hello {name}</h1>
     <ul>
-      <li>Item1 </li>
+      <li style={customStyle}>Item1 </li>
       <li>Item2 </li>
     </ul>
   </div>,
