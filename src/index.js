4aaaf8a4930f5c0d5f4973aab77261bd95b1d752
Dheeraj <54492177+drj09@users.noreply.github.com>
2022-06-27 23:08:17 +0530

    added CSS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 const name = "Drake";
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
+    <h1 className="heading">Hello {name}</h1>
     <ul>
       <li>Item1 </li>
       <li>Item2 </li>
